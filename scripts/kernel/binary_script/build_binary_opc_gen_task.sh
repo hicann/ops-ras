b1993f85b3c5649c4cc1229cb2fb2f7a60f97da6
cann-robot <songchangxia@cann.team>
2026-03-25 20:09:52 +0800

    codecheck告警清理
    Co-authored-by: sangzhenguo<sangzhenguo@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !15 merge master into master
    
    codecheck告警清理
    
    Created-by: sangzhenguo
    Commit-by: sangzhenguo
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    codecheck告警清理
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-ras!15

--- scripts/kernel/binary_script/build_binary_opc_gen_task.sh
+++ scripts/kernel/binary_script/build_binary_opc_gen_task.sh
@@ -136,7 +136,7 @@ call_write_scripts() {
     return 1
   fi
 
-  local UTIL_DIR="${workdir}/../../util/"
+  local util_dir="${workdir}/../../util/"
 
   TMP_DIR=$(mktemp -d)
   trap 'rm -rf "$TMP_DIR"' EXIT
@@ -152,7 +152,7 @@ call_write_scripts() {
 
 python3 -c "
 import sys,json,os
-sys.path.insert(0, '''$UTIL_DIR''')
+sys.path.insert(0, '''$util_dir''')
 from ascendc_impl_build import write_scripts
 
 tmp_dir = '$TMP_DIR'
--- scripts/package/ops_ras/scripts/ops_ras_custom_install.sh
+++ scripts/package/ops_ras/scripts/ops_ras_custom_install.sh
@@ -8,6 +8,7 @@
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------
+set -euo pipefail
 
 curpath=$(dirname $(readlink -f "$0"))
 SCENE_FILE="${curpath}""/../scene.info"
@@ -17,7 +18,7 @@ common_func_path="${curpath}/common_func.inc"
 . "${common_func_path}"
 # init arch
 architecture=$(uname -m)
-architectureDir="${architecture}-linux"
+architecturedir="${architecture}-linux"
 
 while true; do
     case "$1" in
