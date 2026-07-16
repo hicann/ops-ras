01ece8ef2ca30cebdecc56ab1119c898bcd0add2
LiuZonggu <liuzonggu1@huawei.com>
2026-07-16 14:19:18 +0800

    Update repo metadata and compliance config and Remove conv3d_custom example code
    Co-authored-by: LiuZonggu<liuzonggu1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !56 merge repo-metadata-compliance into master
    
    chore: update repo metadata and compliance config
    
    Created-by: LiuZonggu
    Commit-by: LiuZonggu
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    Update repo metadata and configs
    Remove Conv3DV2 example from repo
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    Related to [#3](https://gitcode.com/cann/ops-ras/issues/3) 
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：
    
    
    
    See merge request: cann/ops-ras!56

--- examples/fast_kernel_launch_example/ascend_ops/__init__.py
+++ examples/fast_kernel_launch_example/ascend_ops/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
-# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# Copyright (c) 2026 Huawei Technologies Co., Ltd.
 # This program is free software, you can redistribute it and/or modify it under the terms and conditions of
 # CANN Open Software License Agreement Version 2.0 (the "License").
 # Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 
 """Ascend Ops"""
 __version__ = "1.0.0"
-import torch
 
 try:
     from . import _C
@@ -20,5 +19,3 @@ except ImportError as e:
     raise ImportError(
         "Cannot import _C. Please make sure the `ascend_ops` is properly installed. "
     ) from e
-
-from . import ops
